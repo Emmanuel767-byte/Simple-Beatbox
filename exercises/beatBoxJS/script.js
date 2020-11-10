38c96005252c68c6ac75f98fafc8756764e0e9f6
Emmanuel767-byte <armandeg411@gmail.com>
2020-11-10 00:03:37 -0400

    upload

--- exercises/beatBoxJS/script.js
+++ exercises/beatBoxJS/script.js
@@ -1,6 +1,6 @@
 /**
- * TODO: Create a keydown listener to track what keys are hit
- * TODO: Create a Beat class to represent the beat object in utils.js
+ * DONE: Create a keydown listener to track what keys are hit
+ * DONE: Create a Beat class to represent the beat object in utils.js
  * TODO: Complete triggerBeat() to play upon the press of a,s,d,f,g,h,j,k,l
  * TODO: Button pt1: Initialize color and element values
  * TODO: Button pt2: Set button color upon initialization | Initialize button in beats["65"]
@@ -15,33 +15,43 @@
  * 8, 9: #FFFFFF
  */
 
+
 let beats = {
     "65": {
         beat: new Beat("./assets/Piano Chord 331.mp3"),
+        button: new Button("#00fffe", 65)
     },
     "83": {
         beat: new Beat("./assets/Piano Chord 209.mp3"),
+        button: new Button("#00fffe", 83)
     },
     "68": {
         beat: new Beat("./assets/Piano Chord 208.mp3"),
+        button: new Button("#00fffe", 68)
     },
     "70": {
         beat: new Beat("./assets/Drum Sticks Hit 3.mp3"),
+        button: new Button("#FF00FF", 70)
     },
     "71": {
         beat: new Beat("./assets/Drum Snare Roll.mp3"),
+        button: new Button("#FF00FF", 71)
     },
     "72": {
         beat: new Beat("./assets/PREL Musical 57.mp3"),
+        button: new Button("#FF00FF", 72)
     },
     "74": {
         beat: new Beat("./assets/Cymbal Suspended 2.mp3"),
+        button: new Button("#FFFFFF", 74)
     },
     "75": {
         beat: new Beat("./assets/Musical Compos 33.mp3"),
+        button: new Button("#FFFFFF", 75)
     },
     "76": {
         beat: new Beat("./assets/Musical Orches 4.mp3"),
+        button: new Button("#FFFFFF", 76)
     }
 }
 
@@ -50,10 +60,21 @@ let beats = {
  * HINT: use the keyCode
  */
 triggerBeat = (event) => {
+   const PRESSCODE = event.keyCode;
 
+   if(PRESSCODE in beats){
+       console.log("its working");
+       let valid_keypress= beats[PRESSCODE];//GET the OBJECT(audio object) based on keycode(PRESSCODE)
+       valid_keypress.beat.play()
+       valid_keypress.button.select()
+       
+       
+   }
+   console.log(PRESSCODE);
 }
 
 /**
  * Keydown listener to fire triggerBeat function
  * HINT: Log the keyCode of the key
  */
+document.addEventListener('keydown', triggerBeat);//YOU can parse in a function right awaay for the addeventlISTENER
--- exercises/beatBoxJS/utils.js
+++ exercises/beatBoxJS/utils.js
@@ -3,7 +3,31 @@
  * HINT: Make sure to pass in the audioSrc as parameter to create a new audio track
  * HINT: Create a play function to play the audio if called 
  */
+
+ /*In JAVASCRIPT
+new Audio("")  = is a function that accepts a url/src string and becomes /can creat an AUDIO Object
+example
+let Testrun=  new Audio("");//Audio object created
+Testrun.play()//Audio Object Playing
+
+{// call in function response
+B
+}
+ */
 class Beat {
+    constructor (audioSRC){//Create stting parameter to parse in 
+        this.audio= new Audio(audioSRC)//This audio object creates a new Audio using/receiving 
+         //the audiosrc
+        //create object that passes in the audioSRC everytime
+        
+    }
+play=()=>{
+this.audio.currentTime= 0;
+//sets playback speed. allowing audio to be played as many times as the button is pressed. 
+//instead of waiting for audio to finish play befor initalizing another audio action
+ this.audio.play()
+    
+}
 }
 
 
@@ -12,24 +36,47 @@ class Beat {
  * Button class that keeps track of the button color based on a press
  */
 class Button {
-    constructor(color, keyCode){
+    constructor(color, PRESSCODE){
+        this.color= color;
+        this.PRESSCODE= PRESSCODE;
+        this.element= document.getElementById(PRESSCODE);//Get the element
+        this.setButtonColorInHTML();
+        this.setTransitionListener();
+    
     }
 
     /**
      * Set the button color based on color specified
      */
     setButtonColorInHTML = () => {
+this.element.style.borderColor=this.color;
     }
 
     /**
      * Select function to set the background color and boxShadow
+     * HINT Box shadow looks like this : 0px 0px 17px 0px #FFFFFF
      */
     select = () => {
+        this.element.style.backgroundColor= this.color;
+        this.element.style.boxShadow= `0px 0px 17px 0px ${this.color}`//make the shadow color specific to any selected button with any color
+
     }
 
+   
     /**
      * Deselect function to reset background color and boxShadow
+     * SOLUTION 1: remove styleon keyUp
+     * SOLUTION 2: wait a certain amount of time to reomove style
+     * SOLUTION 2: react on TRANSITION END
      */
+setTransitionListener=()=>{
+    this.element.addEventListener('transitionend', ()=>{
+        this.deselect();
+    })
+}
+
     deselect = () => {
+    this.element.style.backgroundColor="transparent";
+    this.element.style.boxShadow="none";
     }
 }
